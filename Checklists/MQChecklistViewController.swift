b1333cdc451ca385fcd78fb3a012f5e5f5ee5706
MichaelQiu <qiuxiaolei1992@163.com>
2016-01-22 22:59:31 -0800

    构造初步的tableView模型

---
+++ Checklists/MQChecklistViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by MichaelQiu on 16/1/21.
+//  Copyright © 2016年 MichaelQiu. All rights reserved.
+//
+
+import UIKit
+
+class MQChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100;
+    }
+    
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCellWithIndetifier("ChecklistItem", forIndexPath: indexPath)
+//        return cell
+//    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            if cell.accessoryType == UITableViewCellAccessoryType.None {
+                cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            } else {
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            }
+        }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+}
+
