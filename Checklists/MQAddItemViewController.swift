fda210eae75fe45be4482969515deac337ce6428
MichaelQiu <qiuxiaolei1992@163.com>
2016-02-17 22:27:21 -0800

    修改文本框信息

--- Checklists/MQAddItemViewController.swift
+++ Checklists/MQAddItemViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class MQAddItemViewController: UITableViewController {
+protocol MQAddItemViewControllerDelegate: class {
+    func addItemViewControllerDidCancel(controller: MQAddItemViewController)
+    func addItemViewController(controller: MQAddItemViewController, didFinishAddingItem item:MQChecklistItem)
+}
+
+class MQAddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: MQAddItemViewControllerDelegate?
     
     @IBAction func cancel() {
-        dismissViewControllerAnimated(true, completion: nil)
+        delegate?.addItemViewControllerDidCancel(self)
     }
     
     @IBAction func done() {
-        dismissViewControllerAnimated(true, completion: nil)
+        let item = MQChecklistItem()
+        item.text = textField.text!
+        item.checked = false
+        
+        delegate?.addItemViewController(self, didFinishAddingItem: item)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
         return nil
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text!
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        doneBarButton.enabled = (newText.length > 0)
+        return true
     }
 }
